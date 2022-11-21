dd033683fa9e5b5c467fc8ada59d1abd9d143f36
BhanuTejaswi005 <118725619+BhanuTejaswi005@users.noreply.github.com>
2022-11-21 16:32:38 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,19 @@
+valid_dig=[0,1,2,5,6,8,9]
+def valid(k):
+    k=str(k)
+    for i in k:
+        if int(i) not in valid_dig:
+            return False
+    return True
+
+n = int(input())
+if n < len(valid_dig):
+    print(valid_dig[n-1])
+else:
+    count=len(valid_dig)
+    k=valid_dig[-1]
+    while(count<=n):
+        k+=1
+        if valid(k):
+            count+=1
+    print(k)
